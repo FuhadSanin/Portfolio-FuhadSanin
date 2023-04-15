c84123ee788cc8c8899fb293f6c2121f9b4662e0
FuhadSanin <fuhadsanin500@gmail.com>
2023-04-15 15:16:06 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,30 @@ import { Navbar } from "./components/Navbar/Navbar"
 import { Vanta } from "./pages/Banner/Vanta"
 import Portfolio from "./pages/Portfolio/Portfolio"
 import About from "./pages/About/About"
+import { useState, useEffect } from "react"
+import PropagateLoader from "react-spinners/PropagateLoader"
 export const App = () => {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
   return (
-    <div>
-      <Navbar />
-      <Vanta />
-      <Portfolio />
-      <About />
+    <div className="App">
+      {loading ? (
+        <div className="loader">
+          <PropagateLoader color={"#00abf0"} loading={loading} size={30} />
+        </div>
+      ) : (
+        <div className="margin">
+          <Navbar />
+          <Vanta />
+          <Portfolio />
+          <About />
+        </div>
+      )}
     </div>
   )
 }
