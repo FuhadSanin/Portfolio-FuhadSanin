f3b2bda0a1581cda8582cf364ee185931b6f2c1f
FuhadSanin <fuhadsanin500@gmail.com>
2023-04-15 15:08:54 +0530

    portfolio added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import './App.css'
-import {Navbar} from './components/Navbar/Navbar'
-import {Vanta} from './pages/Banner/Vanta'
+import React from "react"
+import "./App.css"
+import { Navbar } from "./components/Navbar/Navbar"
+import { Vanta } from "./pages/Banner/Vanta"
+import Portfolio from "./pages/Portfolio/Portfolio"
 export const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Vanta/>
+      <Navbar />
+      <Vanta />
+      <Portfolio/>
     </div>
   )
 }
